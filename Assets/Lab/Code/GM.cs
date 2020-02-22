76c3212d69e971f5791e154251c876ebc89cb91d
Richard Leinfellner <richard@modulo17.com>
2020-02-22 14:54:15 +0000

    Ready for class

---
+++ Assets/Lab/Code/GM.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //So we can use UI system
+
+
+#pragma warning disable CS0649  //Stop the warning about no assignment, as it will be assigned in IDE
+
+public class GM : MonoBehaviour
+{
+
+    [SerializeField]
+    Text ScoreText; //Link in IDE
+
+    static GM sSingleton;
+
+    int mScore;
+
+    private void Awake()
+    {
+        if (sSingleton == null) //Create Singleton
+        {
+            sSingleton = this;
+            DontDestroyOnLoad(gameObject);
+            Debug.Assert(ScoreText!=null,"Score Text needs to be linked in IDE");
+            StartGame();
+
+        } else if (sSingleton!=this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void StartGame()
+    {
+        Score = 0;
+    }
+    
+    public static  int Score {      //Using a Setter to set core in Canvas whenever it changes
+        get {
+            return sSingleton.mScore;   //Get Score
+        }
+        set {
+            sSingleton.mScore = value;
+            sSingleton.ScoreText.text = string.Format("Score:{0}", sSingleton.mScore);
+        }
+    }
+}
---
+++ Assets/Lab/Code/Pickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#pragma warning disable CS0649  //Stop the warning about no assignment, as it will be assigned in IDE
+
+public class Pickup : MonoBehaviour
+{
+    public float Speed=360.0f;
+    
+    // Add RB & set it to Kinematic
+    void Start()
+    {
+        Rigidbody tRB=gameObject.AddComponent<Rigidbody>();
+        Debug.Assert(tRB != null, "Cannot attach RB, may be duplicate, so remove any added in IDE");
+        tRB.isKinematic = true; //Make it non physics
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Speed * Time.deltaTime, 0);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogFormat("{1} Triggered by {0}", other.gameObject.name,gameObject.name);        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.LogFormat("{1} Collided with {0}", collision.gameObject.name,gameObject.name);
+    }
+
+}
